fda73492c3e4600f11e41a59db881691bdd1b68b
Isa Ali <isaali93@gmail.com>
2017-04-20 19:39:52 -0400

    Initial commit

---
+++ app/src/main/java/com/sample/hrv/C0010R.java
@@ -0,0 +1,80 @@
+package com.sample.hrv;
+
+/* renamed from: com.sample.hrv.R */
+public final class C0010R {
+
+    /* renamed from: com.sample.hrv.R.attr */
+    public static final class attr {
+    }
+
+    /* renamed from: com.sample.hrv.R.drawable */
+    public static final class drawable {
+        public static final int ic_launcher = 2130837504;
+    }
+
+    /* renamed from: com.sample.hrv.R.id */
+    public static final class id {
+        public static final int connection_state = 2131099651;
+        public static final int data_value = 2131099652;
+        public static final int demo = 2131099654;
+        public static final int device_address = 2131099650;
+        public static final int device_name = 2131099661;
+        public static final int device_rssi = 2131099660;
+        public static final int gatt_services_list = 2131099655;
+        public static final int gl = 2131099648;
+        public static final int heartrate_value = 2131099653;
+        public static final int menu_connect = 2131099665;
+        public static final int menu_disconnect = 2131099666;
+        public static final int menu_refresh = 2131099662;
+        public static final int menu_scan = 2131099663;
+        public static final int menu_stop = 2131099664;
+        public static final int modes = 2131099656;
+        public static final int name = 2131099657;
+        public static final int seek = 2131099659;
+        public static final int text = 2131099649;
+        public static final int uuid = 2131099658;
+    }
+
+    /* renamed from: com.sample.hrv.R.layout */
+    public static final class layout {
+        public static final int actionbar_indeterminate_progress = 2130903040;
+        public static final int demo_opengl = 2130903041;
+        public static final int gatt_services_characteristics = 2130903042;
+        public static final int listitem_characteristic = 2130903043;
+        public static final int listitem_device = 2130903044;
+        public static final int listitem_service = 2130903045;
+    }
+
+    /* renamed from: com.sample.hrv.R.menu */
+    public static final class menu {
+        public static final int gatt_scan = 2131034112;
+        public static final int gatt_services = 2131034113;
+    }
+
+    /* renamed from: com.sample.hrv.R.string */
+    public static final class string {
+        public static final int action_demo = 2130968588;
+        public static final int app_name = 2130968576;
+        public static final int ble_not_supported = 2130968577;
+        public static final int connected = 2130968584;
+        public static final int disconnected = 2130968585;
+        public static final int error_bluetooth_not_supported = 2130968587;
+        public static final int label_data = 2130968578;
+        public static final int label_device_address = 2130968580;
+        public static final int label_heartrate = 2130968579;
+        public static final int label_services = 2130968582;
+        public static final int label_state = 2130968581;
+        public static final int menu_connect = 2130968595;
+        public static final int menu_disconnect = 2130968596;
+        public static final int menu_scan = 2130968597;
+        public static final int menu_stop = 2130968598;
+        public static final int no_data = 2130968583;
+        public static final int title_demo_accelerometer = 2130968589;
+        public static final int title_demo_gyroscope = 2130968590;
+        public static final int title_demo_heartrate = 2130968591;
+        public static final int title_devices = 2130968586;
+        public static final int unknown_characteristic = 2130968593;
+        public static final int unknown_device = 2130968592;
+        public static final int unknown_service = 2130968594;
+    }
+}
